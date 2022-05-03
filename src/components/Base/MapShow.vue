cc4023a1bc70ad9191b14d627ebf9445d95d38d5
maple <1574984428@qq.com>
2022-05-03 22:37:22 +0800

    服务器数据

--- src/components/Base/MapShow.vue
+++ src/components/Base/MapShow.vue
@@ -21,8 +21,8 @@ import { onMounted } from 'vue';
 import { storeToRefs } from 'pinia';
 import { useModelStore } from '@/store/model';
 
-import getStationData from '../MapShow/childComps/js/getStationData.js';
-import getSatelliteData from '../MapShow/childComps/js/getSatelliteData.js';
+// import getStationData from '../MapShow/childComps/js/getStationData.js';
+// import getSatelliteData from '../MapShow/childComps/js/getSatelliteData.js';
 
 const {ctx} = useCurrentInstance()
 const path = $computed(() => ctx.$router.currentRoute.value.path)
@@ -31,8 +31,14 @@ const modelStore = useModelStore()
 let {stationData, satelliteData} = storeToRefs(modelStore)
 
 onMounted(() => {
-  stationData.value = getStationData()
-  satelliteData.value = getSatelliteData()
+  let stationDataSocket = new WebSocket('ws://47.99.47.235/getStationData')
+  stationDataSocket.addEventListener('message', (event) => {
+    stationData.value = JSON.parse(event.data)
+  })
+  let satelliteDataSocket = new WebSocket('ws://47.99.47.235/getSatelliteData')
+  satelliteDataSocket.addEventListener('message', (event) => {
+    satelliteData.value = JSON.parse(event.data)
+  })
 })
 </script>
 
