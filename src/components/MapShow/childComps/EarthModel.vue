9238c78f5ccbe3a54a1fc7ff87f20194b2b4d52a
夜刀小茗 <1660378230@qq.com>
2022-05-05 19:25:55 +0800

    feat: EarthModel代码逻辑优化

--- src/components/MapShow/childComps/EarthModel.vue
+++ src/components/MapShow/childComps/EarthModel.vue
@@ -3,19 +3,25 @@
 </template>
 
 <script setup>
-import { onMounted, watch } from "vue"
 import * as echarts from "echarts"
+import { useModelStore } from "@/store/model.js"
+import { useOrbitalStore } from '@/store/orbital'
+import { earthOption } from './js/getEarthOption'
+import circleData from "./js/circleData.js"
 
-const props = defineProps({
-  satelliteData: Array,
-  option: Object,
-})
+const { satelliteData } = $(useModelStore())
+const { filter } = $(useOrbitalStore())
+
 let earthEchart
-let option = props.option
 
-onMounted(() => {
+// 初始化echart实例
+const initEarthEchart = () => {
   earthEchart = echarts.init(document.getElementById("earth"))
-  earthEchart.setOption(option)
+  earthEchart.setOption(earthOption)
+}
+
+// TODO 不知道干嘛的
+const initHandleClick = () => {
   earthEchart.on("click", (params) => {
     if (params.componentSubType === "scatter3D") {
       if (params.data.no[params.data.no.length - 1] === "*") {
@@ -25,15 +31,73 @@ onMounted(() => {
       }
     }
   })
+}
+
+// 设置轨道数据
+const setOrbitalData = () => {
+   earthEchart.setOption({
+    series: [
+      {
+        name: "low",
+        data: circleData.lowCircleData
+      },
+      {
+        name: "middle",
+        data: circleData.middleCircleData
+      },
+      {
+        name: "high",
+        data: circleData.highCircleData
+      }
+    ]
+  })
+}
+
+// 设置卫星数据
+const setSatelliteData = value => {
+  earthEchart.setOption({
+    series: [{
+      name: "satellite",
+      data: value
+    }]
+  })
+}
+
+onMounted(() => {
+  initEarthEchart()
+  initHandleClick()
+  setOrbitalData()
+  setSatelliteData()
 })
 
-watch(
-  () => props.option,
-  (count, precount) => {
-    // console.log(count);
-    earthEchart.setOption(count)
+watch(() => satelliteData, setSatelliteData)
+
+// 基本轨道配置
+const getOrbitaSeries = value => [
+  {
+    name: "low",
+    data: value === "low" ? circleData.lowCircleData : []
+  },
+  {
+    name: "middle",
+    data: value === "middle" ? circleData.middleCircleData : []
+  },
+  {
+    name: "high",
+    data: value === "high" ? circleData.highCircleData : []
   }
-)
+]
+// 轨道线过滤
+watch(() => filter, value => {
+  if(!earthEchart) return;
+  if(value === 'all') {
+    setOrbitalData()
+    setSatelliteData(satelliteData)
+  } else {
+    earthEchart.setOption({ series: getOrbitaSeries(value) })
+    setSatelliteData(satelliteData.filter(i => i.trackType === value))
+  }
+})
 </script>
 
 <style>
